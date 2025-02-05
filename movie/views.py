af166408a43f12432b9968cd338075b6fb6d0d66
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-02-05 11:12:24 -0500

    Cambios en la funcion home nombre Esteban Molina en views.py

--- movie/views.py
+++ movie/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 def home(request):
    # return HttpResponse("<h1>Welcome To The Home Page<h1>")
-   return render(request, "home.html",  {'name':'Greg Lim'})
+   return render(request, "home.html",  {'name':'Esteban Molina'})
