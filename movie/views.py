cbae0f4abead06d35f2f79f3929c0f7687807d5b
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-02-05 18:43:28 -0500

    Html pagina principal

--- movie/views.py
+++ movie/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Movie
 
 def home(request):
    # return HttpResponse("<h1>Welcome To The Home Page<h1>")
-   return render(request, "home.html",  {'name':'Esteban Molina'})
+   # return render(request, "home.html",  {'name':'Esteban Molina'})
+   #searchTerm = request.GET.get("searchMovie")
+   #movies = Movie.objects,all()
+   #return render(request, "home.html", {"searchTerm":searchTerm, "movies": movies})
+    searchTerm = request.GET.get('searchMovie')
+
+    if searchTerm:
+        movies = Movie.objects.filter(title__icontains=searchTerm)
+    else:
+        movies = Movie.objects.all()
+
+    return render(request, 'home.html', {'searchTerm': searchTerm, 'movies': movies})
+
 
 def about(request):
    return render(request, "about.html")
