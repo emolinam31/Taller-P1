c413871f97dc968af23d5abd3f59b3678e90bac4
Esteban Molina Mejia <emolinam@eafit.edu.co>
2025-02-05 18:03:22 -0500

    Importar el Modelo Movie con su metodo get para obtener el par clave valor de el termino buscado con el nombre de la pelicula

--- movie/views.py
+++ movie/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 def home(request):
    # return HttpResponse("<h1>Welcome To The Home Page<h1>")
    return render(request, "home.html",  {'name':'Esteban Molina'})
+
+def about(request):
+   return render(request, "about.html")
